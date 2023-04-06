fbd639e6e410f0927091c56e13a43dbc0fb4cde7
Chijioke Madubuko <105008387+webizone@users.noreply.github.com>
2023-04-06 23:07:08 +0100

    6

---
+++ 0x00-MySQL_Advanced/6-bonus.sql
@@ -0,0 +1,15 @@
+-- A script that creates a stored procedure AddBonus that adds a new correction for a student.
+DELIMITER //
+CREATE PROCEDURE AddBonus(
+	 user_id INTEGER,
+	 project_name VARCHAR(255),
+         score INTEGER
+)
+BEGIN
+    INSERT INTO projects (name)
+    SELECT project_name from DUAL
+    WHERE NOT EXISTS (SELECT * FROM projects WHERE name=project_name LIMIT 1);
+    INSERT INTO corrections (user_id, project_id, score)
+    VALUES(user_id, (SELECT id from projects WHERE name=project_name), score);
+END //
+DELIMITER ;
