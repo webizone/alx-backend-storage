ca6660d17370cd22d181967bd872402303ec3ea6
Chijioke Madubuko <105008387+webizone@users.noreply.github.com>
2023-04-06 22:58:20 +0100

    0

---
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -0,0 +1,7 @@
+-- a script creates a table with the unique users
+DROP TABLE IF EXISTS users;
+CREATE TABLE users (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    name VARCHAR(255)
+);
