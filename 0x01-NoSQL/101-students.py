0c25af37cad81c9b6808dd1a93c6d32987385520
Chijioke Madubuko <105008387+webizone@users.noreply.github.com>
2023-04-11 15:55:01 +0100

    101

---
+++ 0x01-NoSQL/101-students.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""
+Contains a function that returns all students sorted
+by average score
+"""
+
+
+def top_students(mongo_collection):
+    """
+    Returns all students sorted by average score
+
+    Args:
+        mongo_collection: A pymongo collection object
+    Returns:
+        Students sorted by average score
+    """
+    return mongo_collection.aggregate([{'$project': {'name': '$name',
+                                        'averageScore': {'$avg': '$topics.score'}}},
+                                       {'$sort': {'averageScore': -1}}])
